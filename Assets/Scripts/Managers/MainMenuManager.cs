1bb5cfe8c14262c10a6141e930d005e41aa4360f
MaFIaTH <mafeeyawat@gmail.com>
2025-01-26 13:59:08 +0700

    Audio And Aspect Ratio

--- Assets/Scripts/Managers/MainMenuManager.cs
+++ Assets/Scripts/Managers/MainMenuManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
+using MoreMountains.Tools;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -10,11 +11,13 @@ public class MainMenuManager : MonoBehaviour
     [SerializeField] private Transform logo;
     [SerializeField] private Button startButton;
     [SerializeField] private Button leaderboardButton;
+    [SerializeField] private AudioClip bgm;
     private void Start()
     {
         BopLogo();
         startButton.onClick.AddListener(StartGame);
         leaderboardButton.onClick.AddListener(ToLeaderboard);
+        GlobalSoundManager.Instance.PlayBGM("MainMenu", duration: 0.5f);
     }
     private void BopLogo()
     {
